87958a1827b380e8c6989fb9b7bd378e67acf61b
drahmeterdem <138732762+drahmeterdem@users.noreply.github.com>
2025-09-25 14:24:54 +0300

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -5,15 +5,15 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 
-// TODO: Aşağıdaki yapılandırmayı Firebase projenizdeki ayarlarla değiştirin.
-// Firebase konsolunda, Proje Ayarları > Genel sekmesinde bulabilirsiniz.
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyYOUR_API_KEY_HERE",
-  authDomain: "your-project-id.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project-id.appspot.com",
-  messagingSenderId: "your-sender-id",
-  appId: "1:your-sender-id:web:your-app-id"
+  apiKey: "AIzaSyBMOhPCumWJncjfch4GhdPEnwO03c_8o5E",
+  authDomain: "cbt-sim-projesi.firebaseapp.com",
+  projectId: "cbt-sim-projesi",
+  storageBucket: "cbt-sim-projesi.firebasestorage.app",
+  messagingSenderId: "869396190469",
+  appId: "1:869396190469:web:c6db6adefbd2c17e86d36c",
+  measurementId: "G-9S9PYC74LR"
 };
 
 // Firebase'i başlat
